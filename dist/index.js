75874947fc7ec61a44d7b1b75aaf6a40077a625f
John Smith <viz.ivo@gmail.com>
2018-01-08 21:00:42 +0100

    chore: release 1.0.0-alpha.1

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var feedback_1 = require("./lib/feedback");
+exports.Feedback = feedback_1.Feedback;
+//# sourceMappingURL=index.js.map
